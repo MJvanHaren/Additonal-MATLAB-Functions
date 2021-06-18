5d5c0dd04e66f6c065cc094ec3a806ddd90ad94a
Max van Haren <maxvanharen@live.nl>
2021-06-18 13:43:41 +0200

    Initial commit

---
+++ General Functions/coolbode.m
@@ -0,0 +1,48 @@
+function coolbode(varargin)
+    % G = system (ss,transfer function,...)
+    % arg = either empty (normal bode) or 'mag'
+    % win = frequency grid in
+    G = varargin{1};
+    both = true;
+    win = logspace(0,3,2000)*2*pi;
+    if nargin == 2
+        arg = varargin{2};
+        if strcmp(arg,'mag')
+            both = false;
+        else
+            disp('Only magnitude bode are supported (''mag''), plotting default magnitude&phase bode')
+        end
+    elseif nargin == 3
+        arg = varargin{2};
+        if strcmp(arg,'mag')
+            both = false;
+        else
+            disp('Only magnitude bode are supported (''mag''), plotting default magnitude&phase bode')
+        end
+        win = varargin{3};
+    elseif nargin > 4
+        error('Please specify a maximum of 3 inputs');
+    end
+    
+    [mag,phase,wout] = bode(G,win);
+    wout = wout/2/pi;
+    if both
+        subplot(2,1,1);
+    end
+    semilogx(wout,20*log10(squeeze(mag)),'Linewidth',1.5);
+    xlabel('Frequency [Hz]');
+    ylabel('Magnitude [dB]');
+%     grid on; 
+    hold on;
+    xlim([min(wout) max(wout)]);
+    if both
+        subplot(2,1,2);
+        semilogx(wout,squeeze(phase),'Linewidth',1.5);
+        xlabel('Frequency [Hz]')
+        ylabel('Phase [deg]');
+%         grid on; 
+        hold on;
+        xlim([min(wout) max(wout)]);
+    end
+end
+
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Additonal MATLAB Functions
+# Additonal MATLAB Functions
+
+## usage
+ 1. pull to your pc, put it into FOLDER of your choice
+ 2. start MATLAB
+ 3. put in command window 'addpath(genpath((FOLDER/Additonal MATLAB Functions))'
+ 4. put in command window 'savepath'
+
+## File structure
+ 1. General Functions - Containing several nice functions:
+	- coolbode.m - makes bode using semilogx, therefore it is easier to make Latex fonts etc.
+	- MagInset(Ex).m - can be used to create 'zoomed in' sections of a plot
+	- make4.m & profile4.m - can be used for fourth order motion profiles
+	- MatlabDefaultPlotColors.m - returns the 7 default plot colors matlab uses
+	- PlotTrialData(MIMO).m - used for plotting trial data (for ILC)
+	- SetPlotLatexStyle.m - automatically makes fonts/fontsize/renderer of all upcoming plots Latex style
+ 2. GPML - GPML toolbox from Rasumussen
